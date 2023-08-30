0a00501bbb809791a2a32af382d6a7e925f0db4c
dkaitantzidis <dkaitantzidis@byte.gr>
2023-08-30 14:10:01 +0300

    Lesson 151 finished.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit, OnDestroy {
+export class EditServerComponent
+  implements OnInit, OnDestroy, CanComponentDeactivate
+{
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
   allowEdit: boolean = false;
+  changesSaved: boolean = false;
 
   constructor(
     private serversService: ServersService,
     private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -24,7 +30,11 @@ export class EditServerComponent implements OnInit, OnDestroy {
       this.allowEdit = queryParams['allowEdit'] === '1';
     });
     this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -34,7 +44,25 @@ export class EditServerComponent implements OnInit, OnDestroy {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   ngOnDestroy(): void {}
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+
+    if (
+      (this.serverName !== this.server.name ||
+        this.serverStatus !== this.server.status) &&
+      !this.changesSaved
+    ) {
+      return confirm('Do you want to discard the changes?');
+    } else {
+      return false;
+    }
+  }
 }
